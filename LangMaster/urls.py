3ee545232afbc616c1cce93c5d022562fc07bc66
jeong_ahn <ahnjj.dev@gmail.com>
2023-10-02 16:26:40 +0900

    login,out,singup created 1002

--- LangMaster/urls.py
+++ LangMaster/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.views.generic import TemplateView
 
@@ -8,7 +9,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', login_required(TemplateView.as_view(template_name='index.html')), name='index'),  # login 해야지만 접속 가능하도록
 ]
 
 if settings.DEBUG:
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,26 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
+
+login = LoginView.as_view(template_name='accounts/login_form.html')
+
+def logout(request):
+    messages.success(request, '로그아웃되었습니다.')
+    return  logout_then_login(request)   # logout한 다음 바로 login페이지로 넘기기
+    
 
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
+        if form.is_valid():   # get 인자
+            signup_user = form.save()
+            # form.save()
+            auth_login(request, signup_user)
             messages.success(request, "회원가입 환영합니다.")
-            return redirect("/")
+            next_url = request.GET.get('next','/')  # 회원가입시 이동할 주소 : 홈
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
